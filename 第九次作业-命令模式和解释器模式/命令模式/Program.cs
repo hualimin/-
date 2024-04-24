24adbeeff7027d560b8da5fa9e25e4c08e1bac3b
hualimin <69671793+hualimin@users.noreply.github.com>
2024-04-24 17:51:42 +0800

    第九次作业-命令模式和解释器模式

---
+++ 第九次作业-命令模式和解释器模式/命令模式/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 命令模式
+{
+   abstract  class Command
+    {
+        public abstract void Execute();
+    }
+    class Keypad
+    {
+        private Command command;
+        private string name;
+
+        public Keypad(string name,Command cmd)
+        {
+            this.name = name;
+            this.command = cmd;
+        }
+        public Command Command
+        {
+            get { return command; }
+            set { command = value; }
+        }
+
+        public void Click()
+        {
+            Console.WriteLine("点击{0}键！",name);
+            command.Execute();
+        }
+    }
+    class PlayCommand: Command
+    {
+        private PlayClass pc;
+        public PlayCommand()
+        {
+            pc= new PlayClass();
+        }
+        public override void Execute()
+        {
+            pc.Play();
+        }
+    }
+
+    class RewindCommand : Command
+    {
+        private RewindClass rc;
+        public RewindCommand()
+        {
+            rc = new RewindClass();
+        }
+        public override void Execute()
+        {
+            rc.Rewind();
+        }
+    }
+    class StopCommand : Command
+    {
+        private StopClass sc;
+        public StopCommand()
+        {
+            sc = new StopClass();
+        }
+        public override void Execute()
+        {
+            sc.Stop();
+        }
+    }
+    class PlayClass
+    {
+        public void Play()
+        {
+            Console.WriteLine("播放磁带！");
+            Console.WriteLine("啊八八八八");
+        }
+    }
+
+    class RewindClass
+    {
+        public void Rewind()
+        {
+            Console.WriteLine("倒放磁带！");
+            Console.WriteLine("八八八八啊");
+        }
+    }
+
+    class StopClass
+    {
+        public void Stop()
+        {
+            Console.WriteLine("暂停磁带！");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Keypad playk, rewindk, stopk;
+            Command play, rewind, stop;
+            play = new PlayCommand();
+            rewind = new RewindCommand();
+            stop = new StopCommand();
+
+            playk = new Keypad("播放",play);
+            rewindk = new Keypad("倒带", rewind);
+            stopk = new Keypad("暂停", stop);
+
+            playk.Click();
+            stopk.Click();
+            rewindk.Click();
+
+            Console.Read();
+        }
+    }
+}
---
+++ 第九次作业-命令模式和解释器模式/解释器模式/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace 解释器模式
+{
+    abstract class AbstractNode
+    {
+        public abstract int Interpret();
+    }
+
+    class NumNode:AbstractNode
+    {
+        private int num;
+
+        public NumNode(string num)
+        {
+            this.num = Convert.ToInt32(num);
+        }
+        public override int Interpret()
+        {
+            return num;
+        }
+    }
+    class AddNode : AbstractNode
+    {
+        private AbstractNode left; 
+        private AbstractNode right; 
+
+        public AddNode(AbstractNode left, AbstractNode right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public override int Interpret()
+        {
+            return left.Interpret() + right.Interpret();
+        }
+    }
+
+    class SubNode : AbstractNode
+    {
+        private AbstractNode left; 
+        private AbstractNode right; 
+        public SubNode(AbstractNode left, AbstractNode right)
+        {
+            this.left = left;
+            this.right = right;
+        }
+        public override int Interpret()
+        {
+            return left.Interpret() - right.Interpret();
+        }
+    }
+
+    class Handler
+    {
+        private AbstractNode node;
+
+        public void Handle(string opeation)
+        {
+            AbstractNode left = null, right = null;
+            Stack stack = new Stack(); //声明一个栈对象用于存储抽象语法树
+            string[] nums = opeation.Split(' '); //以空格分隔指令字符串
+            for (int i = 0; i < nums.Length; i++)
+            {  		        
+                if (nums[i].Equals("+"))
+                {
+                    string nextnum = nums[++i];
+                    left = (AbstractNode)stack.Pop();
+                    right = new NumNode(nextnum);
+                    stack.Push(new AddNode(left, right));
+                }
+                else if (nums[i].Equals("-"))
+                {
+                    string nextnum = nums[++i];
+                    left = (AbstractNode)stack.Pop();
+                    right = new NumNode(nextnum);
+                    stack.Push(new SubNode(left,right));
+                }
+                else
+                {
+                    string num = nums[i];
+                    left = new NumNode(num);
+                    stack.Push(left);
+                }
+            }
+            this.node = (AbstractNode)stack.Pop(); //将全部表达式从栈中弹出
+        }
+
+        public int Output()
+        {
+            int result = node.Interpret(); //解释表达式
+            return result;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入操作：");
+            string opera = Console.ReadLine();
+            Handler hand = new Handler();
+            hand.Handle(opera);
+            int result = hand.Output();
+            Console.WriteLine("结果是：{0}",result);
+            Console.Read();
+        }
+    }
+}
