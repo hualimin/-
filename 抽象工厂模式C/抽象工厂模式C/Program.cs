cd2210b82f333efaff1d42a228b25bfd41c37fa7
hualimin <69671793+hualimin@users.noreply.github.com>
2024-04-24 17:53:07 +0800

    抽象工厂模式C

---
+++ 抽象工厂模式C/抽象工厂模式C/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+
+namespace 抽象工厂模式C
+{
+    public interface IFruit
+    {
+        void Display();
+    }
+
+    class Apple : IFruit
+    {
+        public void Display()
+        {
+            Console.WriteLine("种子结果了，得到了一个苹果！");
+        }
+    }
+
+    class Orange : IFruit
+    {
+        public void Display()
+        {
+            Console.WriteLine("种子结果了，得到了一个橘子！");
+        }
+    }
+
+    class Banana : IFruit
+    {
+        public void Display()
+        {
+            Console.WriteLine("种子结果了，得到了一个香蕉！");
+        }
+    }
+
+    public class FruitFactory
+    {
+        public static IFruit CreateFactory(string seedName)
+        {
+            IFruit fruit = null;
+            switch (seedName)
+            {
+                case "appleseed":
+                    fruit = new Apple();
+                    break;
+
+                case "orangeseed":
+                    fruit = new Orange();
+                    break;
+
+                case "bananaseed":
+                    fruit = new Banana();
+                    break;
+            }
+            return fruit;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seedname;
+            seedname = ConfigurationManager.AppSettings["seed"];
+            IFruit fruit;
+            // Console.WriteLine("请输入要种下的种子");
+            //seedname = Console.ReadLine();
+            // fruit = FruitFactory.CreateFactory(seedname);
+            fruit = (IFruit)Assembly.Load("抽象工厂模式C").CreateInstance("抽象工厂模式C." + seedname);
+            fruit.Display();
+            Console.Read();
+        }
+    }
+}
