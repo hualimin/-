c7ea3cc1faf57f98a6b6835f97a63df060b4f379
hualimin <69671793+hualimin@users.noreply.github.com>
2024-04-24 17:41:40 +0800

    第5次作业-适配器及桥接器模式

---
+++ 第五次作业-适配器及桥接器模式/桥接器模式/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Reflection;
+
+namespace 桥接器模式
+{
+    class Program
+    {
+        abstract class MusicImp
+        {
+            public abstract void Load();
+        }
+
+        class WindowsImp:MusicImp
+        {
+            string name = "Windows";
+            public override void Load()
+            {
+                Console.WriteLine("在"+name+"系统下加载：");
+            }
+        }
+
+        class LinuxImp : MusicImp
+        {
+            string name = "Linux";
+            public override void Load()
+            {
+                Console.WriteLine("在" + name + "系统下加载：");
+            }
+        }
+
+        class UnixImp : MusicImp
+        {
+            string name = "Unix";
+            public override void Load()
+            {
+                Console.WriteLine("在" + name + "系统下加载：");
+            }
+        }
+
+
+        abstract class Music
+        {
+            protected MusicImp imp;
+
+            public void SetMusicImp(MusicImp imp)
+            {
+                this.imp = imp;
+            }
+
+            public abstract void Play(string fileName);
+        }
+
+        class Mp3Music : Music
+        {
+            string type = "MP3";
+            public override void Play(string fileName)
+            {
+                imp.Load();
+                Console.WriteLine("播放"+type+"格式音乐："+fileName);
+            }
+        }
+
+        class WavMusic : Music
+        {
+            string type = "Wav";
+            public override void Play(string fileName)
+            {
+                imp.Load();
+                Console.WriteLine("播放" + type + "格式音乐：" + fileName);
+            }
+        }
+
+        class WmaMusic : Music
+        {
+            string type = "Wma";
+            public override void Play(string fileName)
+            {
+                imp.Load();
+                Console.WriteLine("播放" + type + "格式音乐：" + fileName);
+            }
+        }
+
+        class ApeMusic : Music
+        {
+            string type = "Ape";
+            public override void Play(string fileName)
+            {
+                Console.WriteLine("播放" + type + "格式音乐：" + fileName);
+            }
+        }
+        static void Main(string[] args)
+        {
+            MusicImp imp = new LinuxImp();
+            Music music = new Mp3Music();
+
+            //string type = ConfigurationManager.AppSettings["Type"];
+            //string system = ConfigurationManager.AppSettings["System"];
+            //MusicImp imp = (MusicImp)Assembly.Load("桥接器模式").CreateInstance("桥接器模式." + system);
+            //Music music = (Music)Assembly.Load("桥接器模式").CreateInstance("桥接器模式." + type);
+
+            music.SetMusicImp(imp);
+
+            music.Play("两只老虎");
+            Console.Read();
+
+        }
+    }
+}
---
+++ 第五次作业-适配器及桥接器模式/适配器模式/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.Configuration;
+
+namespace 适配器模式
+{
+    //方案一
+    public interface Adaptee
+    {
+        void Cry();
+        void Move();
+
+        
+    }
+    class Dog : Adaptee
+    {
+
+        public void Cry()
+        {
+            Console.WriteLine("汪汪汪！");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("狗在跑！");
+        }
+    }
+
+    class Bird : Adaptee
+    {
+        public void Cry()
+        {
+            Console.WriteLine("叽叽叽！");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("鸟在飞！");
+        }
+    }
+
+    class Adaptor: Robort
+    {
+        Adaptee adaptee;
+
+        public void SetDuiXiang(Adaptee adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+        public void Cry()
+        {
+            adaptee.Cry();
+        }
+
+        public void Move()
+        {
+            adaptee.Move();
+        }
+    }
+
+    interface Robort
+    {
+        void SetDuiXiang(Adaptee adaptee);
+        void Cry();
+
+        void Move();
+    }
+
+    ////方案二
+    //public class Dog
+    //{
+
+    //    public void Cry()
+    //    {
+    //        Console.WriteLine("汪汪汪！");
+    //    }
+
+    //    public void Move()
+    //    {
+    //        Console.WriteLine("狗在跑！");
+    //    }
+    //}
+
+    //public class Bird
+    //{
+    //    public void Cry()
+    //    {
+    //        Console.WriteLine("汪汪汪！");
+    //    }
+
+    //    public void Move()
+    //    {
+    //        Console.WriteLine("鸟在飞！");
+    //    }
+    //}
+
+    //public interface Robert
+    //{
+    //    void Cry();
+    //    void Move();
+    //}
+
+    //public class Adapter: Robert
+    //{
+    //    Dog dog;
+    //    Bird bird;
+    //    public Adapter(Dog dog)
+    //    {
+    //        this.dog = dog;
+    //    }
+
+    //    public Adapter(Bird bird)
+    //    {
+    //        this.bird = bird;
+    //    }
+    //    public void Cry()
+    //    {
+    //        bird.Cry();
+    //    }
+
+    //    public void Move()
+    //    {
+    //        this.Move();
+    //    }
+    //}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string duixiang = ConfigurationManager.AppSettings["name"].ToString();
+            Adaptee adaptee = (Adaptee)Assembly.Load("适配器模式").CreateInstance("适配器模式." + duixiang);
+            //方案一
+            //Adaptee adaptee = new Dog();
+            Robort robort = new Adaptor();
+            robort.SetDuiXiang(adaptee);
+            robort.Cry();
+            robort.Move();
+
+            ////方案二
+            //Dog dog = new Dog();
+            //Robert robert = new Adapter(dog);
+            //robert.Move();
+
+            Console.Read();
+        }
+    }
+}
