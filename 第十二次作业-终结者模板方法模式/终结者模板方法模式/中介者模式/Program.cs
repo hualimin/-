821540552d4110baa8b52c49af5e21d58fdebc3e
hualimin <69671793+hualimin@users.noreply.github.com>
2024-04-24 17:48:00 +0800

    第十二次作业-终结者模板方法模式

---
+++ 第十二次作业-终结者模板方法模式/终结者模板方法模式/中介者模式/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace 中介者模式
+{
+    abstract class UnitedNations
+    {
+        public abstract void SendMsg(string message, Country colleague);
+    }
+
+    class WHO: UnitedNations
+    {
+        China china;
+        USA usa;
+        Japan Japan;
+
+        public China Colleague1
+        {
+            set { china = value; }
+        }
+        public USA Colleague2
+        {
+            set { usa = value; }
+        }
+        public Japan Colleague3
+        {
+            set { Japan = value; }
+        }
+
+        public override void SendMsg(string message, Country colleague)
+        {
+            if (colleague == china)
+            {
+                usa.GetMsg(message);
+                Japan.GetMsg(message);
+            }
+            else if(colleague == usa)
+            {
+                china.GetMsg(message);
+                Japan.GetMsg(message);
+            }
+            else if (colleague == Japan)
+            {
+                china.GetMsg(message);
+                usa.GetMsg(message);
+            }
+        }
+    }
+
+    abstract class Country
+    {
+        protected UnitedNations unitedNations;
+        public Country(UnitedNations unitedNations)
+        {
+            this.unitedNations = unitedNations;
+        }
+
+    }
+
+    class China:Country
+    {
+        public China(UnitedNations unitedNations) : base(unitedNations) { }
+
+        public void SendMsg(string msg)
+        {
+            unitedNations.SendMsg(msg,this);
+        }
+
+        public void GetMsg(string msg)
+        {
+            Console.WriteLine("中国接到消息："+msg);
+        }
+    }
+
+    class USA : Country
+    {
+        public USA(UnitedNations unitedNations) : base(unitedNations) { }
+
+        public void SendMsg(string msg)
+        {
+            unitedNations.SendMsg(msg, this);
+        }
+
+        public void GetMsg(string msg)
+        {
+            Console.WriteLine("美国接到消息：" + msg);
+        }
+    }
+    class Japan : Country
+    {
+        public Japan(UnitedNations unitedNations) : base(unitedNations) { }
+
+        public void SendMsg(string msg)
+        {
+            unitedNations.SendMsg(msg, this);
+        }
+
+        public void GetMsg(string msg)
+        {
+            Console.WriteLine("日本接到消息：" + msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WHO who = new WHO();
+            China china = new China(who);
+            USA usa = new USA(who);
+            Japan japan = new Japan(who);
+
+            who.Colleague1 = china;
+            who.Colleague2 = usa;
+            who.Colleague3 = japan;
+
+            china.SendMsg("中国疫情得到控制！");
+            usa.SendMsg("美国已经放弃治疗！");
+            japan.SendMsg("日本，你们想不想喝核水？");
+
+            Console.Read();
+        }
+    }
+}
---
+++ 第十二次作业-终结者模板方法模式/终结者模板方法模式/模板方法模式/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 模板方法模式
+{
+    abstract class Bank
+    {
+        public void GetNumber()
+        {
+            Console.WriteLine("排队取号！");
+        }
+
+        abstract public void Business();
+
+        public void Evaluate()
+        {
+            Console.WriteLine("请为本次服务打分！");
+        }
+        public void Handle()
+        {
+            GetNumber();
+            Business();
+            Evaluate();
+        }
+    }
+    class Deposit: Bank
+    {
+        public override void Business()
+        {
+            Console.WriteLine("办理存款业务！");
+        } 
+    }
+
+    class Withdrawal : Bank
+    {
+        public override void Business()
+        {
+            Console.WriteLine("办理取款业务！");
+        }
+    }
+
+    class Transfer : Bank
+    {
+        public override void Business()
+        {
+            Console.WriteLine("办理转账业务！");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bank bank1,bank2,bank3;
+            bank1 = new Withdrawal();
+            bank1.Handle();
+            Console.WriteLine();
+
+            bank2 = new Deposit();
+            bank2.Handle();
+            Console.WriteLine();
+            bank3 = new Transfer();
+            bank3.Handle();
+
+            Console.Read();
+        }
+    }
+}
