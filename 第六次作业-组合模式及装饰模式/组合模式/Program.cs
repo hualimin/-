03a81a98420594e54f7245608bdbe5b210c0e7a6
hualimin <69671793+hualimin@users.noreply.github.com>
2024-04-24 17:50:54 +0800

    第六次作业-组合模式及装饰模式

---
+++ 第六次作业-组合模式及装饰模式/组合模式/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 组合模式
+{
+    class Program
+    {
+        abstract class AbstractSchool
+        {
+            protected string name;
+            protected string content;
+            public AbstractSchool(string name)
+            {
+                this.name = name;
+                content = "";
+            }
+
+            public string Content 
+            {
+                get { return content; }
+                set { content = value; }
+            }
+            public abstract void Add(AbstractSchool depart);
+            public abstract void Remove(AbstractSchool depart);
+            public abstract AbstractSchool GetChild(int i);
+            public abstract void SendFile(int depth);
+        }
+
+        class ConcreteSchool : AbstractSchool
+        {
+            private List<AbstractSchool> children = new List<AbstractSchool>();
+
+            public ConcreteSchool(string name)
+                : base(name)
+            { }
+
+            public override void Add(AbstractSchool c)
+            {
+                children.Add(c);
+            }
+
+            public override void Remove(AbstractSchool c)
+            {
+                children.Remove(c);
+            }
+            public override AbstractSchool GetChild(int i)
+            {
+                return (AbstractSchool)children[i];
+            }
+
+            //下发公文
+            public override void SendFile(int depth)
+            {
+
+                Console.Write(new String('-', depth) + name+"下发公文:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(content);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                foreach (AbstractSchool c in children)
+                {
+                    c.Content = this.content;
+                    c.SendFile(depth + 2);
+                }
+            }    
+
+        }
+
+        class Department : AbstractSchool
+        {
+            public Department(string name)
+                : base(name)
+            { }
+
+            public override void Add(AbstractSchool c)
+            {
+            }
+
+            public override void Remove(AbstractSchool c)
+            {
+            }
+            public override AbstractSchool GetChild(int i)
+            {
+                return null;
+            }
+            //下发公文
+            public override void SendFile(int depth)
+            {
+                Console.Write(new String('-', depth) + name + "收到公文:");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(content);
+                Console.ForegroundColor = ConsoleColor.White;
+            } 
+        }
+        static void Main(string[] args)
+        {
+            AbstractSchool bj, bjjw, bjxz, hn, hnjw, hnxz, cs, csjw, csxz, xt, xtjw, xtxz;
+            bj = new ConcreteSchool("北京总部");
+            bjjw = new Department("教务办公室");
+            bjxz= new Department("行政办公室");
+
+            hn = new ConcreteSchool("湖南分校");
+            hnjw = new Department("湖南教务办公室");
+            hnxz = new Department("湖南行政办公室");
+
+            cs = new ConcreteSchool("长沙分校");
+            csjw = new Department("长沙教务办公室");
+            csxz = new Department("长沙行政办公室");
+
+            xt = new ConcreteSchool("湘潭分校");
+            xtjw = new Department("湘潭教务办公室");
+            xtxz = new Department("湘潭行政办公室");
+
+            bj.Add(bjjw);
+            bj.Add(hn);
+            bj.Add(bjxz);
+
+            hn.Add(hnjw);
+            hn.Add(hnxz);
+
+            hn.Add(cs);
+            hn.Add(xt);
+
+            cs.Add(csjw);
+            cs.Add(csxz);
+
+            xt.Add(xtjw);
+            xt.Add(xtxz);
+
+            
+            string content = "紧急文件";
+            bj.Content = content;
+            bj.SendFile(1);
+
+            Console.Read();
+        }
+    }
+}
---
+++ 第六次作业-组合模式及装饰模式/装饰模式/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 装饰模式
+{
+    class Program
+    {
+        static public int tot_price;
+        abstract class Starbuzz
+        {
+            protected string cfname;
+            protected int price;
+            //public int totot_price;
+            public abstract void Display();
+
+        }
+
+        class HouseBlend : Starbuzz
+        {
+            public HouseBlend()
+            {
+                cfname = "综合";
+                tot_price=price = 25;
+            }
+            public override void Display()
+            {
+                Console.Write("一杯"+cfname + "咖啡,");
+            }
+
+        }
+        class DarkRost : Starbuzz
+        {
+            public DarkRost()
+            {
+                cfname = "深培";
+                tot_price=price = 20;
+            }
+            public override void Display()
+            {
+                Console.Write("一杯" + cfname+"咖啡,");
+            }
+        }
+
+        class Decat : Starbuzz
+        {
+            public Decat()
+            {
+                cfname = "低咖啡因";
+                tot_price=price = 30;
+            }
+            public override void Display()
+            {
+                Console.Write("一杯" + cfname + "咖啡,");
+            }
+        }
+
+        class Espresso : Starbuzz
+        {
+            public Espresso()
+            {
+                cfname = "浓缩";
+                tot_price=price = 25;
+            }
+            public override void Display()
+            {
+                Console.Write("一杯" + cfname + "咖啡,");
+            }
+        }
+
+        class Peiliao : Starbuzz
+        {
+            private Starbuzz coffee;
+
+            public Peiliao(Starbuzz coffee)
+            {
+                this.coffee = coffee;
+            }
+            public override void Display()
+            {
+                coffee.Display();
+            }
+        }
+
+        class Milk:Peiliao
+        {
+            private int price;
+
+            public Milk(Starbuzz cofee):base(cofee)
+            {
+                this.price =6;
+            }
+            public void Add()
+            {
+                //显示
+                Console.Write("加了一份牛奶,");
+                //修改价格
+                tot_price += price;
+            }
+            public override void Display()
+            {
+                base.Display();
+                this.Add();
+            }
+        }
+
+        class Soy : Peiliao
+        {
+            private int price;
+
+            public Soy(Starbuzz cofee) : base(cofee)
+            {
+                this.price = 4;
+            }
+            public void Add()
+            {
+                //显示
+                Console.Write("加了一份豆浆,");
+                //修改价格
+                tot_price += price;
+            }
+            public override void Display()
+            {
+                base.Display();
+                this.Add();
+            }
+        }
+
+        class Mocha : Peiliao
+        {
+            private int price;
+
+            public Mocha(Starbuzz cofee) : base(cofee)
+            {
+                this.price = 10;
+            }
+            public void Add()
+            {
+                //显示
+                Console.Write("加了一份摩卡,");
+                //修改价格
+                tot_price += price;
+            }
+            public override void Display()
+            {
+                base.Display();
+                this.Add();
+            }
+        }
+        class Whip : Peiliao
+        {
+            private int price;
+
+            public Whip(Starbuzz cofee) : base(cofee)
+            {
+                this.price = 8;
+            }
+            public void Add()
+            {
+                //显示
+                Console.Write("加了一份奶泡,");
+                //修改价格
+                tot_price += price;
+            }
+            public override void Display()
+            {
+                base.Display();
+                this.Add();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Starbuzz coffee,milk,soy,mocha,whip;
+            coffee = new Espresso();
+            milk = new Milk(coffee);
+            soy = new Soy(milk);
+            mocha = new Mocha(soy);
+            whip = new Whip(mocha);
+            whip.Display();
+            Console.WriteLine("总计"+tot_price+"元。");
+            Console.Read();
+        }
+    }
+}
